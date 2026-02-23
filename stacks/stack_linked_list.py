b178855a9024f214ac5f9a9f68dce360be9ea1c8
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-23 23:50:52 +0200

    feat: implement constructor and push method to stack

---
+++ stacks/stack_linked_list.py
@@ -0,0 +1,12 @@
+from linked_lists.linked_list import LinkedList
+
+class Stack:
+    def __init__(self):
+        '''this function is constructor to the class Stack'''
+        self.elments: LinkedList = LinkedList()
+    
+    def push(self, data):
+        '''this function append_end data to the stack linked list'''
+        #Time Complexity is O(n)
+        #Space complexity is o(1)
+        self.elments.append_end(data)
