247a2d3c9759c8d040b081a4889056198d640a97
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-23 23:53:24 +0200

    test: add test push unit test and make it pass

---
+++ tests/test_stack_linked_list.py
@@ -0,0 +1,10 @@
+import pytest
+from stacks.stack_linked_list import Stack
+
+def test_push():
+    stack: Stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+
+    assert stack.elments.length == 3
