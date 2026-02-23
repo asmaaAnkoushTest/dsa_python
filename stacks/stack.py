9a2273297b175edc974e0bfc538d626eb0fde6c1
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-23 15:02:53 +0200

    feat: implement push method that push the value to the top of stack

--- stacks/stack.py
+++ stacks/stack.py
@@ -16,18 +16,3 @@ class Stack:
         node.next = self.top
         self.top = node
         self.length += 1
-    
-    def __str__(self):
-        current_top = self.top
-        string: str = "\n"
-        while current_top != -1:
-            string = string + f"{current_top.value}\n"
-            string = string + "^\n"
-            current_top = current_top.next
-        return string
-
-s = Stack()
-s.push(10)
-s.push(100)
-
-print(s.length)
