36dfc5144807c4195ee4a1b635ac069f9c8dc0df
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-23 22:25:40 +0200

    test: add test_pop for stack unit test t and make it pass

--- tests/test_stack_list.py
+++ tests/test_stack_list.py
@@ -1,3 +1,4 @@
+import pytest
 from stacks.stack_list import Stack
 
 def test_push():
@@ -7,3 +8,17 @@ def test_push():
     stack.push(30)
 
     assert stack.length == 3
+
+def test_pop():
+    stack: Stack = Stack()
+    empty_satck: Stack = Stack()
+
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+    poped_element = stack.pop()
+    assert poped_element == 30
+    assert stack.length == 2
+
+    with pytest.raises(IndexError):
+        poped_element = empty_satck.pop()
