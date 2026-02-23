fddb00264fe857ce4e1d26857545518d425c13dd
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-23 15:04:58 +0200

    test: add test_push unit test and make it pass

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -10,3 +10,12 @@ def test_stack_node_creation():
     stack: Stack = Stack()
     assert stack.top is -1
     assert stack.length == 0
+
+def test_push():
+    stack: Stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+
+    assert stack.length == 3
+    assert stack.top.value == 30
