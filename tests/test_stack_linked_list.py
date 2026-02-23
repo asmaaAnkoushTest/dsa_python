bc43eaec0ae561411d35e90ef153e69e0b18ae8f
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-24 00:04:25 +0200

    test: add test_pop unit test and make it pass

--- tests/test_stack_linked_list.py
+++ tests/test_stack_linked_list.py
@@ -8,3 +8,17 @@ def test_push():
     stack.push(30)
 
     assert stack.elments.length == 3
+
+def test_pop():
+    stack: Stack = Stack()
+    empty_satck: Stack = Stack()
+
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+    poped_element = stack.pop()
+    assert poped_element == 30
+    assert stack.elments.length == 2
+
+    with pytest.raises(IndexError):
+        poped_element = empty_satck.pop()
