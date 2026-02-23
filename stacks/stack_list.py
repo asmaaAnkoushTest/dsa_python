218a37365ff83c45c3b7adb3efe2b715cf497761
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-23 22:24:16 +0200

    feat: implement pop to delete and return the last element in stack and _str_ for human-readable representation

--- stacks/stack_list.py
+++ stacks/stack_list.py
@@ -6,6 +6,29 @@ class Stack:
     
     def push(self, data):
         '''this function append data to the stack list'''
+        #Time Complexity is O(1), o(n) if list resized
+        #Space complexity is o(1),, o(n) if list resized
         self.elements.append(data)
         self.length += 1
     
+    def pop(self):
+        '''this function return and delete the last element in stack'''
+        #Time Complexity is O(1).
+        #Space complexity is o(1).
+        if self.elements == []:
+            raise IndexError ("The Stack is Empty")
+        else:
+            poped_element = self.elements[-1]
+            self.elements.remove(self.elements[-1])
+            self.length -= 1
+            return poped_element
+    
+    def __str__(self):
+        '''this function for human-readable representation'''
+        #Time Complexity is O(1)
+        #Space complexity is o(1)
+        string: str = ""
+        for i in range(len(self.elements)-1,-1,-1):
+            string = string + "^\n"
+            string = string + f"{self.elements[i]}\n"
+        return string
