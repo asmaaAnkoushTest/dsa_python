1940b3a79d62269e08e4e3e7fcd217a2cd75590a
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-23 15:31:28 +0200

    feat:add pop method that return and delete the value of the top

--- stacks/stack.py
+++ stacks/stack.py
@@ -7,21 +7,34 @@ class Node:
 class Stack:
     def __init__(self):
         '''this function is constracter to the class Stack'''
-        self.top = -1
+        self.top = None
         self.length = 0
 
     def push(self, data):
         '''this function add data in the top of stack: node point to old top and then updata top'''
+        #Time Complexity is O(1)
         node: Node = Node (data)
         node.next = self.top
         self.top = node
         self.length += 1
     
     def __str__(self):
+        ''''this function for human-readable representation"'''
         current_top = self.top
         string: str = "\n"
-        while current_top != -1:
+        while current_top:
             string = string + f"{current_top.value}\n"
             string = string + "^\n"
             current_top = current_top.next
         return string
+
+    def pop (self):
+        ''''this function remove and return the top element in stack'''
+        #Time Complexity is O(1)
+        if self.top == None:
+            raise IndexError ("The Stack is Empty")
+        else:
+            poped_data = self.top.value
+            self.top = self.top.next
+            self.length -= 1
+            return poped_data
