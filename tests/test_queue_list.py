3c9b3a9474c632e940813a2f8a97b84ca164b66b
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-24 03:16:00 +0200

    test: add test_dequeue unit test and make it pass

--- tests/test_queue_list.py
+++ tests/test_queue_list.py
@@ -8,3 +8,15 @@ def test_enqueue():
     queue.enqueue(20)
     queue.enqueue(30)
     assert queue.length == 3
+
+def test_dequeue():
+    queue: Queue = Queue()
+    empty_queue: Queue = Queue()
+    queue.enqueue(10)
+    queue.enqueue(20)
+    queue.enqueue(30)
+    assert queue.length == 3
+    queue.dequeue()
+    assert queue.length == 2
+    with pytest.raises(IndexError):
+        empty_queue.dequeue()
