cda65a7a18421a4b1e53dc7360a7ac8c9920811d
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-24 03:21:38 +0200

    test: add test_front unit test and make it pass

--- tests/test_queue_list.py
+++ tests/test_queue_list.py
@@ -20,3 +20,14 @@ def test_dequeue():
     assert queue.length == 2
     with pytest.raises(IndexError):
         empty_queue.dequeue()
+
+def test_front():
+    queue: Queue = Queue()
+    queue.enqueue(10)
+    queue.enqueue(20)
+    queue.enqueue(30)
+    assert queue.length == 3
+    assert queue.front() == 10
+    queue.dequeue()
+    assert queue.length == 2
+    assert queue.front() == 20
