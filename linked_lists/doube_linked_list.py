a796034f3cd2750f5d6e779a617b634455e972a1
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-25 00:37:31 +0200

    feat: implement the constructor of Node and DoubleLinkedList classes

--- linked_lists/doube_linked_list.py
+++ linked_lists/doube_linked_list.py
@@ -1,11 +1,13 @@
 class Node:
     def __init__(self, value):
+        '''this function is constructor to the class Node'''
         self.value = value
         self.next = None
         self.prev = None
 
 class DoubleLinkedList:
     def __init__(self):
+        '''this function is constructor to the class Node'''
         self.head = None
         self.length = 0
         
