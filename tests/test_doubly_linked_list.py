15bb08e17c22a7f54528dc1340e22b2db77473cc
AsmaaAnkoushTest <asmaa.h.ankoush@gmail.com>
2026-02-25 02:17:41 +0200

    test: add test_insert_at unit test and make it pass

--- tests/test_doubly_linked_list.py
+++ tests/test_doubly_linked_list.py
@@ -29,3 +29,25 @@ def test_insert_at_tail():
     assert dll.tail.value is 20
     assert dll.length == 3
 
+def test_insert_at():
+    dll: DoublyLinkedList = DoublyLinkedList()
+    dll.insert_at_head(10)
+    dll.insert_at_head(5)
+    assert dll.tail.value is 10
+    assert dll.head.value is 5
+    assert dll.length == 2
+    dll.insert_at(0, 2)
+    assert dll.tail.value is 10
+    assert dll.head.value is 2
+    assert dll.length == 3
+    dll.insert_at(3, 20)
+    assert dll.tail.value is 20
+    assert dll.head.value is 2
+    assert dll.length == 4
+    dll.insert_at(2, 15)
+    assert dll.tail.value is 20
+    assert dll.head.value is 2
+    assert dll.length == 5
+
+    
+
